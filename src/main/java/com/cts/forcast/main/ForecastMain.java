32716fea7a9ab507e6eea2a598bb9ca02f8fed0f
273840 <273840@PC312610.cts.com>
2018-02-15 16:24:23 +0530

    Intial Commit

---
+++ src/main/java/com/cts/forcast/main/ForecastMain.java
@@ -0,0 +1,12 @@
+package com.cts.forcast.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForecastMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForecastMain.class, args);
+	}
+}
