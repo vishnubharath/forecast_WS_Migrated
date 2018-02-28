e5df7cae7c3df4c19c615783b9d8b6f155abe9fc
273840 <273840@PC312610.cts.com>
2018-02-28 20:38:54 +0530

    Adding Cross Origin

--- src/main/java/com/cts/forcast/controller/ProjectController.java
+++ src/main/java/com/cts/forcast/controller/ProjectController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.cts.forcast.domain.project.Project;
 import com.cts.forcast.service.ProjectService;
 
 @Controller
+@CrossOrigin
 @RestController
 @RequestMapping("/projects")
 public class ProjectController {
