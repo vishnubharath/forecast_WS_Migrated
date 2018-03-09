8b819802d83ea4977fc46c528e99ecde6d4d2159
567755 <567755@PC301175.cts.com>
2018-03-09 16:33:00 +0530

    Date format changes

--- src/main/java/com/cts/forcast/dao/report/ReportEntity.java
+++ src/main/java/com/cts/forcast/dao/report/ReportEntity.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "Forecast_Reports")
@@ -63,9 +64,11 @@ public class ReportEntity implements Serializable {
 	private String associateGrade;
 
 	@Column(name = "Alloc_Start_Date")
+	@DateTimeFormat(pattern = "MM/dd/yyyy")
 	private Date allocStartDate;
 
 	@Column(name = "Alloc_End_Date")
+	@DateTimeFormat(pattern = "MM/dd/yyyy")
 	private Date allocEnddate;
 
 	@Column(name = "Project_Billability")
