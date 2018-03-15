cc3c64d47a3514cd4d18ad40e5348c735eb97bf1
567755 <567755@PC301175.cts.com>
2018-03-15 12:30:11 +0530

    Removal of unncessary files

--- src/main/java/com/cts/forcast/common/PersistentFields.java
+++
@@ -1,7 +0,0 @@
-package com.cts.forcast.common;
-
-public interface PersistentFields {
-
-	String PROJECT_ID_FIELD = "project_id";
-	String EMPLOYEE_ID_FIELD = "emp_id";
-}
--- src/main/java/com/cts/forcast/dao/report/ReportAdjustmentEntityPK.java
+++
@@ -1,9 +0,0 @@
-package com.cts.forcast.dao.report;
-
-import java.io.Serializable;
-
-public class ReportAdjustmentEntityPK implements Serializable{
-	private Long associateId;
-	private Long projectId;
-
-}
